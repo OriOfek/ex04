aa1616c03abc02ba3ac64faa5e89f264675938d1
OriOfek <aa6055@bs.amalnet.k12.il>
2020-01-29 09:54:57 +0200

    work

--- app/src/main/java/com/example/ex04/MainActivity.java
+++ app/src/main/java/com/example/ex04/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.ex04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn=(Button)findViewById(R.id.btn);
+        i=1;
     }
-}
+
+    public void change(View view) {
+        if (i%7==0)
+            {
+            btn.setText("boom");
+            }
+        else
+            {
+            btn.setText("This is a click number:"+""+i);
+            }
+        i++;
+        }
+
+    }
